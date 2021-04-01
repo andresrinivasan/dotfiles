6e2e976f90e1340c5381f4d458e56fe1187c78c1
André Srinivasan <andre.srinivasan@gmail.com>
2021-04-01 14:26:22 -0700

    Detect using wired headset
    Signed-off-by: André Srinivasan <andre.srinivasan@gmail.com>

--- hammerspoon/audio.lua
+++ hammerspoon/audio.lua
@@ -9,7 +9,7 @@ local function setDefaultAudioDevice(deviceIn, deviceOut)
   else
     status = "FAILED"
   end
-
+  
   hs.notify.show("Audio", string.format("Switching to %s...", deviceIn:name()), status)
 end
 
@@ -30,17 +30,16 @@ end
 -- hs.audiodevice.watcher.setCallback(audioDeviceChanged)
 -- hs.audiodevice.watcher.start()
 
--- function audiowatch(arg)
---   print(string.format("Audiowatch arg: %s", arg))
--- end
+function audiowatch(arg)
+  print(string.format("Audiowatch arg: %s", arg))
+end
 
--- hs.audiodevice.watcher.setCallback(audiowatch)
--- hs.audiodevice.watcher.start()
+hs.audiodevice.watcher.setCallback(audiowatch)
+hs.audiodevice.watcher.start()
 
 -- Inspired by https://www.tunabellysoftware.com/balance_lock/
 local function resetBalance(uid, event, scope, element)
-  print(event)
-  if event == "span" then
+  if event == "span" or event == "vmvc" then    -- Span should be sufficient per the docs
     device = hs.audiodevice.findDeviceByUID(uid)
     if device:balance() ~= 0.5 then
       device:setBalance(0.5)
@@ -48,29 +47,33 @@ local function resetBalance(uid, event, scope, element)
   end
 end
 
-audioWatcher = {}
-local audio = function() 
-  for i,dev in ipairs(hs.audiodevice.allOutputDevices()) do
-    audioWatcher[dev:name()] = dev:watcherCallback(resetBalance):watcherStart()
-    print(dev:watcherIsRunning())
+local function selectJack(uid, event, scope, element)
+  if event =="jack" then
+    if builtinIn:jackConnected() then
+      setDefaultAudioDevice(builtinIn, builtinOut)
+    end
   end
-  
-  for i,dev in ipairs(hs.audiodevice.allOutputDevices()) do
-    print(dev:watcherIsRunning())
-  end
-  
-  hs.hotkey.bind(ctrl_opt, "f7", function()
-    setDefaultAudioDevice(link370In, link370Out)
-  end)
-  
-  hs.hotkey.bind(ctrl_opt, "f8", function()
-    hs.notify.show("Audio", "Default audio device", hs.audiodevice.defaultInputDevice():name())
-  end)
-  
-  hs.hotkey.bind(ctrl_opt, "f9", function()
-    setDefaultAudioDevice(builtinIn, builtinOut)
-  end)
 end
-audio()
 
-return audio
+for i,dev in ipairs(hs.audiodevice.allOutputDevices()) do
+  dev:watcherCallback(resetBalance):watcherStart()
+  print(dev:watcherIsRunning())
+end
+
+for i,dev in ipairs(hs.audiodevice.allOutputDevices()) do   -- XXX Bug. Prints false.
+  print(dev:watcherIsRunning())
+end
+
+builtinIn:watcherCallback(selectJack):watcherStart()
+
+hs.hotkey.bind(ctrl_opt, "f7", function()
+  setDefaultAudioDevice(link370In, link370Out)
+end)
+
+hs.hotkey.bind(ctrl_opt, "f8", function()
+  hs.notify.show("Audio", "Default audio device", hs.audiodevice.defaultInputDevice():name())
+end)
+
+hs.hotkey.bind(ctrl_opt, "f9", function()
+  setDefaultAudioDevice(builtinIn, builtinOut)
+end)
