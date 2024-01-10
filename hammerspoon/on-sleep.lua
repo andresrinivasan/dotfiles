cb576d27fc9481e7104865556562f149073b6b0d
André Srinivasan <andre.srinvasan@gmail.com>
2024-01-10 09:26:11 -0800

    Cleanup
    Signed-off-by: André Srinivasan <andre.srinvasan@gmail.com>

--- hammerspoon/on-sleep.lua
+++ hammerspoon/on-sleep.lua
@@ -1,15 +1,24 @@
 -- From https://gist.github.com/ysimonson/fea48ee8a68ed2cbac12473e87134f58
 
-function googleOneVPNOff()
+local module = {}
+
+local function googleOneVPNOff()
     local t = hs.task.new("/usr/sbin/scutil", nil, {"--nc", "stop", "48797392-C894-4002-BD1E-16308CC9FC97"})
     t:start()
 end
 
-function f(event)
+local function decaffeinate()
+    spoon.Caffeine:setState(false)
+end
+
+local function f(event)
     if event == hs.caffeinate.watcher.systemWillSleep then
         googleOneVPNOff()
+        decaffeinate()
     end
 end
 
-watcher = hs.caffeinate.watcher.new(f)
-watcher:start()
+module.watcher = hs.caffeinate.watcher.new(f)
+module.watcher:start()
+
+return module
