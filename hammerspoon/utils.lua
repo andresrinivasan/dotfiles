e1e6d08cbe9d53ff76c92fd003a2428d1c5cb686
André Srinivasan <andre.srinivasan@gmail.com>
2024-07-02 22:58:29 -0700

    Cleanup
    Signed-off-by: André Srinivasan <andre.srinivasan@gmail.com>

--- hammerspoon/utils.lua
+++ hammerspoon/utils.lua
@@ -1,8 +1,12 @@
 -- From https://github.com/zzamboni/dot-hammerspoon/blob/master/init.lua
 
+local module = {}
+
 -- Returns the bundle ID of an application, given its path.
-function appID(app)
+function module:appID(app)
     if hs.application.infoForBundlePath(app) then
       return hs.application.infoForBundlePath(app)['CFBundleIdentifier']
     end
 end
+
+return module
