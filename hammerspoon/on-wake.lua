973ddd63c6a5e8153cac35e739380f04552cbf84
André Srinivasan <andre.srinivasan@gmail.com>
2024-07-08 15:54:42 -0700

    Cleanup re-launch Toggl Track on wake
    Signed-off-by: André Srinivasan <andre.srinivasan@gmail.com>

--- hammerspoon/on-wake.lua
+++ hammerspoon/on-wake.lua
@@ -11,21 +11,6 @@ local utils = require("utils")
 
 local togglTrackApp = utils:appID("/Applications/Toggl Track.app")
 
--- Suppress warning about Spotlight
-hs.application.enableSpotlightForNameSearches(false)
-
-local function healthCheckApp(hint, checkWindow)
-    return function()
-        if hs.application.get(hint) == nil then
-            return false
-        elseif checkWindow and hs.application.get(hint):focusedWindow() == nil then
-            return false
-        else
-            return true
-        end
-    end
-end
-
 local function hideApp(hint)
     return function(timer)
         hs.application.get(hint):focusedWindow():close()
@@ -35,38 +20,34 @@ end
 local function launchApp(hint)
     return function(timer)
         hs.application.launchOrFocusByBundleID(hint)
-        hs.timer.waitUntil(healthCheckApp(hint, true), hideApp(hint))
+        hs.timer.waitUntil(function()
+            if hs.application.get(hint) ~= nil and hs.application.get(hint):focusedWindow() ~= nil then
+                return true
+            else
+                return false
+            end
+        end, hideApp(hint))
     end
 end
 
 local function maybeRestartTogglTrack()
     local a = hs.application.get(togglTrackApp)
     if a ~= nil then
-        a:kill9()
-        hs.timer.waitWhile(healthCheckApp(togglTrackApp, false), launchApp(togglTrackApp))
+        a:kill()
+        hs.timer.waitUntil(function()
+            if hs.application.get(hint) == nil then
+                return true
+            else
+                return false
+            end
+        end, launchApp(togglTrackApp))
     end
-end
-
--- Assumes sudo doesn't require a password for renice
---
--- Eg. 
--- ALL ALL = NOPASSWD: /usr/bin/renice
-
-local function pgrep(exitCode, stdOut, stdErr)
-    local p = string.gsub(stdOut, "%s+", "")
-    local t = hs.task.new("/usr/bin/sudo", nil, {"/usr/bin/renice", "20", p})
-    t:start()
-end
-
-local function renice(name)
-    local t = hs.task.new("/usr/bin/pgrep", pgrep, {name})
-    t:start()
 end
     
 local function f(event)
     if event == hs.caffeinate.watcher.systemDidWake then
         maybeRestartTogglTrack()
-        renice("crowdstrike")
+        utils:renice("crowdstrike", 20)
     end
 end
 
