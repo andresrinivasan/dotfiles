4dcf6ae74333a08bdcde5f99d203cef3a375b0f3
André Srinivasan <andre.srinivasan@gmail.com>
2024-11-19 19:08:21 -0800

    Playing
    Signed-off-by: André Srinivasan <andre.srinivasan@gmail.com>

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -6,6 +6,38 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
+-- config.unix_domains = {
+--   {
+--     name = 'unix',
+--   },
+-- }
+-- local mux = wezterm.mux
+
+-- -- Decide whether cmd represents a default startup invocation
+-- function is_default_startup(cmd)
+--   if not cmd then
+--     -- we were started with `wezterm` or `wezterm start` with
+--     -- no other arguments
+--     return true
+--   end
+--   if cmd.domain == "DefaultDomain" and not cmd.args then
+--     -- Launched via `wezterm start --cwd something`
+--     return true
+--   end
+--   -- we were launched some other way
+--   return false
+-- end
+
+-- wezterm.on('gui-startup', function(cmd)
+--   if is_default_startup(cmd) then
+--     -- for the default startup case, we want to switch to the unix domain instead
+--     local unix = mux.get_domain("unix")
+--     mux.set_default_domain(unix)
+--     -- ensure that it is attached
+--     unix:attach()
+--   end
+-- end)
+
 config.window_frame = {
   border_left_width = '0.1cell',
   border_right_width = '0.1cell',
